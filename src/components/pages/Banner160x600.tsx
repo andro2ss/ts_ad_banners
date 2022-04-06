63513118d0a6be9ce0249e2b79b592e374ca6c6f
andro2ss <a.brzeski@hotmail.com>
2022-04-06 18:45:36 +0200

    added banner component

--- src/components/pages/Banner160x600.tsx
+++ src/components/pages/Banner160x600.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import Banner160X600Px from "../common/banner160x600/Banner160x600px";
 
 function Banner160X600() {
-    return (
-        <div>2
-                <Link to="/" className="linkToTask__link--home">
-                    Cofnij
-                </Link>
-        </div>
-    );
+  return (
+    <div>
+      <Banner160X600Px />
+      <Link to="/" className="linkToTask__link--home">
+        Cofnij
+      </Link>
+    </div>
+  );
 }
 
 export default Banner160X600;
